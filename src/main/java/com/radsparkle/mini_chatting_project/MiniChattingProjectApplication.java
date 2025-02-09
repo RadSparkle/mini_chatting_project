6d965d9531199bbad996942507a0354c2917f527
RadSparkle <wwwartg1@naver.com>
2025-02-09 22:00:53 +0900

    Initial commit

---
+++ src/main/java/com/radsparkle/mini_chatting_project/MiniChattingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.radsparkle.mini_chatting_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniChattingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniChattingProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/radsparkle/mini_chatting_project/MiniChattingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.radsparkle.mini_chatting_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniChattingProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
